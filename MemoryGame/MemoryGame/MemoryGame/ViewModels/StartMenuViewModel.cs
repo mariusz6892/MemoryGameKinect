915ab1d1ada168dd7d68518d3f354adf63482e20
Mariusz Bińkowski <mariusz6892@gmail.com>
2018-12-17 19:46:35 +0100

    Initial commit

---
+++ MemoryGame/MemoryGame/MemoryGame/ViewModels/StartMenuViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame.ViewModels
+{
+    public class StartMenuViewModel
+    {
+        private MainWindow _mainWindow;
+        public StartMenuViewModel(MainWindow main)
+        {
+            _mainWindow = main;
+        }
+
+        public void StartNewGame(int slidecount)
+        {
+            GameViewModel newGame = new GameViewModel(slidecount);
+            _mainWindow.DataContext = newGame;
+        }
+    }
+}
---
+++ MemoryGame/MemoryGame/MemoryGame/Views/StartMenuView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using MemoryGame.ViewModels;
+using System;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit.Input;
+using Microsoft.Kinect.Wpf.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryGame.Views
+{
+    /// <summary>
+    /// Interaction logic for StartMenuView.xaml
+    /// </summary>
+    public partial class StartMenuView : UserControl
+    {
+
+        public StartMenuView()
+        {
+            InitializeComponent();
+            KinectRegion.SetKinectRegion(this, KinectRegionXAML);
+            KinectRegionXAML.Loaded += KinectRegionXAML_Loaded;
+            KinectRegionXAML.KinectSensor = KinectSensor.GetDefault();
+            KinectRegionXAML.KinectSensor.Open();
+        }
+
+        private void Play_Clicked_Easy(object sender, RoutedEventArgs e)
+        {
+            var startMenu = DataContext as StartMenuViewModel;
+            startMenu.StartNewGame(4);
+        }
+        private void Play_Clicked_Medium(object sender, RoutedEventArgs e)
+        {
+            var startMenu = DataContext as StartMenuViewModel;
+            startMenu.StartNewGame(5);
+        }
+        private void Play_Clicked_Hard(object sender, RoutedEventArgs e)
+        {
+            var startMenu = DataContext as StartMenuViewModel;
+            startMenu.StartNewGame(6);
+        }
+        private void KinectRegionXAML_Loaded(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Kinect Loaded");
+        }
+    }
+}
